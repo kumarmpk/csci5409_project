4baac846318f7e30bd6a39670635a029d2f2c688
Ruize Nie <rz376775@dal.ca>
2020-07-08 14:20:46 -0300

    Company Y: adding the docker file to deploy the web server

---
+++ compY/build/precache-manifest.b32ec3193ceb6ce5d23735d1062f29bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3f32e0e042dce472469403a36734439",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b935bce4247e4b53dda2",
+    "url": "/static/css/main.8f3e6f13.chunk.css"
+  },
+  {
+    "revision": "6229f3c344a249af4f0c",
+    "url": "/static/js/2.6caf04de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6caf04de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b935bce4247e4b53dda2",
+    "url": "/static/js/main.49450f94.chunk.js"
+  },
+  {
+    "revision": "5ee6184fbc6ad61e4f16",
+    "url": "/static/js/runtime-main.3ef5615c.js"
+  }
+]);
